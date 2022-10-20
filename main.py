6fa1a86aeb2a34290ed71af433d45e9f4f0237d3
Danila <danilaphiliptsow@gmail.com>
2022-10-20 23:45:20 +0300

    Update main 1

--- main.py
+++ main.py
@@ -44,9 +44,6 @@ def column_with_alignment(strs, align_type='left'):
     return column[:-1]
 
 
-str_to_center
-
-
 class Table:
     def __init__(self,
                  rows=0,
@@ -68,7 +65,7 @@ class Table:
         self.arr = self.create_arr()
 
     def create_arr(self):
-        return [[self.new_cell()] * self.columns] * self.rows
+        return [[self.new_cell() for i in range(self.columns)] for j in range(self.rows)]
 
     def new_cell(self,
                  content="",
@@ -111,30 +108,47 @@ class Table:
         if cell_type != self.arr[row_num - 1][col_num - 1]["type"] and cell_type is not None:
             self.arr[row_num - 1][col_num - 1]["type"] = cell_type
         if cell_format != self.arr[row_num - 1][col_num - 1]["format"] and cell_format is not None:
-            self.arr[row_num - 1][col_num - 1]["format"] = cell_format
+            self.arr[row_num - 1][col_num - 1]["format"] = cell_format  # todo
 
     #         if row_sep != self.arr[row_num - 1][col_num - 1]["row_sep"] and row_sep is not None:
     #             self.arr[row_num - 1][col_num - 1]["row_sep"] = row_sep
     #         if col_sep != self.arr[row_num - 1][col_num - 1]["col_sep"] and col_sep is not None:
     #             self.arr[row_num - 1][col_num - 1]["col_sep"] = col_sep
 
+    def edit_arr(self,
+                 content_arr=((None,),),
+                 size_arr=((None,),),
+                 align_arr=((None,),),
+                 cell_type_arr=((None,),),
+                 cell_format_arr=((None,),)  # ,
+                 # row_sep_arr=None,
+                 # col_sep_arr=None
+                 ):
+        for row in range(
+                max([len(content_arr), len(size_arr), len(align_arr), len(cell_type_arr), len(cell_format_arr)])):
+            for column in range(max([len(content_arr[0]), len(size_arr[0]), len(align_arr[0]), len(cell_type_arr[0]),
+                                     len(cell_format_arr[0])])):
+                self.edit_cell(row + 1, column + 1,
+                               content=None if content_arr == ((None,),) else content_arr[row][column],
+                               size=None if size_arr == ((None,),) else size_arr[row][column],
+                               align=None if align_arr == ((None,),) else align_arr[row][column],
+                               cell_type=None if cell_type_arr == ((None,),) else cell_type_arr[row][column],
+                               cell_format=None if cell_format_arr == ((None,),) else cell_format_arr[row][column])
+
     def cell_to_string(self, row, col):
         return self.arr[row][col]["content"]
 
     def show(self):
         res_str = ""
-        for row in range(self.size()[1]):
-            for col in range(self.size()[0]):
+        for row in range(self.rows):
+            for col in range(self.columns):
                 res_str += self.cell_to_string(row, col)
             res_str += "\n"
-        return res_str
-
-    def size(self):
-        return [self.columns, self.rows]
+        return res_str[:-1]
 
 
 table1 = Table(rows=2, columns=3)
-table1.edit_cell(1, 1, content="a")
+table1.edit_arr(content_arr=[["11", "12", "13"], ["21", "22", "23"]], size_arr=[["10", "10", "10"], ["10", "10", "10"]])
 print(table1.show())
-print(table1.arr)
-print("size is", table1.size())
+print(f"size is [{table1.columns}, {table1.rows}]")
+print("\n\n\n")
